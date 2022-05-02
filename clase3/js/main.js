dc9008b329ca669bc3e2d844253a3668eab29f75
Juan Gerardo <102968010+Jmendezzz@users.noreply.github.com>
2022-05-01 20:25:22 -0500

    parcial

---
+++ clase3/js/main.js
@@ -0,0 +1,14 @@
+// let num=prompt("ingrese un numero"); //  num="10" --> num=10;
+// num= parseInt(num);
+// let num2=prompt("ingrese el segundo numero");
+// num2=parseInt(num2);
+// let suma = num + num2;
+// document.write(suma);
+
+// if((suma>10 && suma<20) || (suma<10 && suma <0) ){
+//     alert("La suma es mayor que 10 y menor que 20");
+// }else if(suma>20 && suma<30){
+//     alert("La suma es mayor que 20 y menor q 30");
+// }else alert("Ninguna de las condiciones se cumplió");
+
+
